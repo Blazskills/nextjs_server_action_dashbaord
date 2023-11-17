93f3d27498fd10feed4501850bbe5f2ae2dcee97
Blazskills <ilesanmiisaac@gmail.com>
2023-11-17 02:21:34 +0100

    updated homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
       <div>
-        Home
+        <Link href="/dashboard">
+        Dashboard
+        </Link>
       </div>
     </main>
   )
